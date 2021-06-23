f2a0111f52644690884aeeab2dc40a07407626af
onepieceze <onepieceze@163.com>
2021-06-23 16:44:36 +0800

    remove trash

--- tools/write_geogrid/src/write_geogrid.i
+++
@@ -1,8 +0,0 @@
-%module write_geogrid
-
-
-%{
-extern int write_geogrid(char * input, int isfile, int number, int subset); 
-%}
-
-extern int write_geogrid(char * input, int isfile, int number, int subset);
--- tools/write_geogrid/test.sh
+++
@@ -1,5 +0,0 @@
-cd /Users/xiezm/Project/wind_farm/modis_package/tools/write_geogrid/src
-rm write_geogrid.py write_geogrid_wrap.c
-swig -python write_geogrid.i
-cd /Users/xiezm/Project/wind_farm/modis_package/tools/write_geogrid
-python3 setup.py install
