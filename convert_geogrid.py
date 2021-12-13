3dfe6ff46d3898a84396e835f33baa7e649a2e6f
onepieceze <onepieceze@163.com>
2021-12-13 11:10:52 +0800

    fixed bugs

--- convert_geogrid.py
+++ convert_geogrid.py
@@ -1,36 +1,3 @@
-#! /usr/local/bin/python3
-#! -*- coding: utf-8 -*-
-#import argparse
-#import os
-#import sys
-#from array import array
-#script_root = os.path.dirname(os.path.realpath(__file__))
-#sys.path.append(f'{os.path.dirname(os.path.realpath(__file__))}/utils')
-#from utils import cli
-#try:
-#  from write_geogrid import write_geogrid
-#except ImportError:
-#  os.chdir(f'{script_root}/tools/write-geogrid')
-#  status = os.system('python3 setup.py install')
-#  if status != 0: raise  ImportError('Install write_grid Failed.')
-#  from write_geogrid import write_geogrid
-#  os.chdir(script_root)
-#
-#def write(inputs, subset):
-#
-#  if os.path.isfile(inputs):
-#    number = 1
-#  else:
-#    if not os.path.exists(inputs):
-#      cli.error("file or directory not exist.")
-#      exit()
-#    number = len(os.listdir(inputs))
-#    if not number > 1:
-#      cli.error("directory is empty.")
-#      exit()
-#
-#  write_geogrid(inputs, os.path.isfile(inputs), number, subset)
-
 import argparse
 from python_geogrid import geogrid
 try:
@@ -52,30 +19,30 @@ def convert(inputs, subset):
   ysize       = ds.RasterYSize
   
   in_band = ds.GetRasterBand(subset)
-  data    = in_band.ReadAsArray()
-  
+  data    = in_band.ReadAsArray()[::-1, :]
+
   out = geogrid("write")
   
-  out.set_index(key="type"        , value="categorical")
-  out.set_index(key="category_min", value=1)
-  out.set_index(key="category_min", value=20)
-  out.set_index(key="projection"  , value="regular_ll")
-  out.set_index(key="dx"          , value=geotranform[1])
-  out.set_index(key="dy"          , value=abs(geotranform[5]))
-  out.set_index(key="known_x"     , value=1)
-  out.set_index(key="known_y"     , value=1)
-  out.set_index(key="known_lat"   , value=geotranform[3]+geotranform[5]*ysize)
-  out.set_index(key="known_lon"   , value=geotranform[0])
-  out.set_index(key="wordsize"    , value=1)
-  out.set_index(key="tile_x"      , value=xsize)
-  out.set_index(key="tile_y"      , value=ysize)
-  out.set_index(key="tile_z"      , value=1)
-  out.set_index(key="units"       , value="\"category\"")
-  out.set_index(key="description" , value="\"MODIS modified-IGBP landuse - 500 meter\"")
-  out.set_index(key="mminlu"      , value="\"MODIFIED_IGBP_MODIS_NOAH\"")
-  out.set_index(key="iswater"     , value=17)
-  out.set_index(key="isice"       , value=15)
-  out.set_index(key="isurban"     , value=13)
+  out.set_index(key="type"         , value="categorical")
+  out.set_index(key="category_min" , value=1)
+  out.set_index(key="category_max" , value=20)
+  out.set_index(key="projection"   , value="regular_ll")
+  out.set_index(key="dx"           , value=geotranform[1])
+  out.set_index(key="dy"           , value=abs(geotranform[5]))
+  out.set_index(key="known_x"      , value=1)
+  out.set_index(key="known_y"      , value=1)
+  out.set_index(key="known_lat"    , value=geotranform[3]+geotranform[5]*ysize)
+  out.set_index(key="known_lon"    , value=geotranform[0])
+  out.set_index(key="wordsize"     , value=1)
+  out.set_index(key="tile_x"       , value=xsize)
+  out.set_index(key="tile_y"       , value=ysize)
+  out.set_index(key="tile_z"       , value=1)
+  out.set_index(key="units"        , value="\"category\"")
+  out.set_index(key="description"  , value="\"MODIS modified-IGBP landuse - 500 meter\"")
+  out.set_index(key="mminlu"       , value="\"MODIFIED_IGBP_MODIS_NOAH\"")
+  out.set_index(key="iswater"      , value=17)
+  out.set_index(key="isice"        , value=15)
+  out.set_index(key="isurban"      , value=13)
   
   index_root = "./"
 
